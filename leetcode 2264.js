0f0e7a23b6fab57ac23437a72ce93a40e7973daf
Hariharan <146021544+Hariharimass1205@users.noreply.github.com>
2024-06-25 10:08:59 +0530

    Update leetcode 2264.js

--- leetcode 2264.js
+++ leetcode 2264.js
@@ -15,7 +15,27 @@ while(i<num.length){
    
     i++
 }
-
 return result
 };
 
+
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestGoodInteger = function(num) {
+ const arr  =  num.split("")
+     let aa=[]
+for(i=0;i<arr.length;i++){
+    if(arr[i]==arr[i+1] && arr[i+1]==arr[i+2] ){
+           aa.push(`${arr[i] + arr[i+1] + arr[i+2]}`)
+    }  
+}
+console.log(aa,aa.length)
+if(aa.length==0) return ""
+if(aa.length==1) return aa[0]
+//if(aa[0]==aa[1]) return aa[0]
+let aaa = Math.max(...aa)
+if(aaa=="0") return "000"
+return aaa.toString()
+};
